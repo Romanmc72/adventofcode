1297f4d63675de2082ad37caa0e02783434a0461
Roman Czerwinski <romanczerwinski@r0m4n.com>
2024-12-03 22:28:38 -0600

    Got day 3!

--- 2024/days/day_03.go
+++ 2024/days/day_03.go
@@ -3,6 +3,9 @@ package days
 import (
 	_ "embed"
 	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
 
 	"github.com/Romanmc72/adventofcode/2024/util"
 )
@@ -23,10 +26,62 @@ func Solve03(part int, example bool) error {
 	}
 	logger.Debug(data)
 	if part < 1 || part == 1 {
-		fmt.Println("Day 03 Part 1 Solution: _____")
+		exp := regexp.MustCompile(`mul\(\d+,\d+\)`)
+		found := exp.FindAllString(data, -1)
+		total := 0
+		for _, each := range found {
+			val, err := parseMulExpression(each)
+			if err != nil {
+				return err
+			}
+			total += val
+		}
+		fmt.Println("Day 03 Part 1 Solution: ", total)
 	}
 	if part < 1 || part == 2 {
-		fmt.Println("Day 03 Part 2 Solution: _____")
+		exp := regexp.MustCompile(`(mul\(\d+,\d+\)|do\(\)|don't\(\))`)
+		found := exp.FindAllString(data, -1)
+		total := 0
+		do := true
+		for _, each := range found {
+			if isMul(each) {
+				if do {
+					val, err := parseMulExpression(each)
+					if err != nil {
+						return err
+					}
+					total += val
+				}
+			} else {
+				do = parseDosAndDonts(each)
+			}
+		}
+		fmt.Println("Day 03 Part 1 Solution: ", total)
 	}
 	return nil
+}
+
+func parseDosAndDonts(s string) bool {
+	return s == "do()"
+}
+
+func isMul(s string) bool {
+	return s[0] == 'm'
+}
+
+func parseMulExpression(exp string) (int, error) {
+	sides := strings.Split(exp, ",")
+	left := sides[0]
+	right := sides[1]
+	leftSides := strings.Split(left, "(")
+	rightSides := strings.Split(right, ")")
+	leftNum, err := strconv.Atoi(leftSides[1])
+	if err != nil {
+		return 0, err
+	}
+	rightNum, err := strconv.Atoi(rightSides[0])
+	if err != nil {
+		return 0, err
+	}
+	return leftNum * rightNum, nil
 }
