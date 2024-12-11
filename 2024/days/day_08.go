540faf402716edf0e6e285b9f9dd2307af7bc544
Roman Czerwinski <romanczerwinski@r0m4n.com>
2024-12-11 15:48:08 -0600

    got day 8 and wrote some tests along the way!

--- 2024/days/day_08.go
+++ 2024/days/day_08.go
@@ -3,6 +3,7 @@ package days
 import (
 	_ "embed"
 	"fmt"
+	"strings"
 
 	"github.com/Romanmc72/adventofcode/2024/util"
 )
@@ -14,19 +15,122 @@ var real08Data []byte
 var example08Data []byte
 
 func Solve08(part int, example bool) error {
-	logger := util.GetLogger()
 	var data string
 	if example {
 		data = string(example08Data)
 	} else {
 		data = string(real08Data)
 	}
-	logger.Debug(data)
+	uniqueAntinodeLocations := make(map[util.Coordinate]bool)
+	uniqueAntinodeFrequencyLocations := make(map[util.Coordinate]bool)
+	antennae, mapBounds := parseAntennae(data)
+	for _, antennaeLocations := range antennae {
+		for index, loc := range antennaeLocations {
+			if index == (len(antennaeLocations) - 1) {
+				break
+			}
+			for i := index + 1; i < len(antennaeLocations); i++ {
+				nextLoc := antennaeLocations[i]
+				antinodes := getAntinodeLocations(loc, nextLoc)
+				for _, a := range antinodes {
+					if isWithinBounds(a, mapBounds) {
+						uniqueAntinodeLocations[a] = true
+					}
+				}
+				antinodes = getAntinodeFrequencyLocations(loc, nextLoc, mapBounds)
+				for _, a := range antinodes {
+					if isWithinBounds(a, mapBounds) {
+						uniqueAntinodeFrequencyLocations[a] = true
+					}
+				}
+			}
+		}
+	}
 	if part < 1 || part == 1 {
-		fmt.Println("Day 08 Part 1 Solution: _____")
+		fmt.Println(showAntennae(antennae, uniqueAntinodeLocations, mapBounds))
+		fmt.Println("Day 08 Part 1 Solution:", len(uniqueAntinodeLocations))
 	}
 	if part < 1 || part == 2 {
-		fmt.Println("Day 08 Part 2 Solution: _____")
+		fmt.Println(showAntennae(antennae, uniqueAntinodeFrequencyLocations, mapBounds))
+		fmt.Println("Day 08 Part 2 Solution:", len(uniqueAntinodeFrequencyLocations))
 	}
 	return nil
+}
+
+func showAntennae(antennae map[rune][]util.Coordinate, antinodes map[util.Coordinate]bool, bounds util.Coordinate) string {
+	m := make([][]string, bounds.Y)
+	for y := 0; y < bounds.Y; y++ {
+		row := make([]string, bounds.X)
+		for x := 0; x < bounds.X; x++ {
+			row[x] = "."
+		}
+		m[y] = row
+	}
+	for a := range antinodes {
+		m[a.Y][a.X] = "#"
+	}
+	for c, locations := range antennae {
+		for _, l := range locations {
+			m[l.Y][l.X] = string(c)
+		}
+	}
+	s := make([]string, bounds.Y)
+	for i, r := range m {
+		s[i] = strings.Join(r, "")
+	}
+	return strings.Join(s, "\n")
+}
+
+func isWithinBounds(c util.Coordinate, b util.Coordinate) bool {
+	return c.X >= 0 && c.X < b.X && c.Y >= 0 && c.Y < b.Y
+}
+
+// slope = (x1 - x2) / (y1 - y2)
+func getCoordinateDelta(a util.Coordinate, b util.Coordinate) util.Coordinate {
+	return util.Coordinate{X: a.X - b.X, Y: a.Y - b.Y}
+}
+
+func getAntinodeLocations(a util.Coordinate, b util.Coordinate) []util.Coordinate {
+	delta := getCoordinateDelta(a, b)
+	return []util.Coordinate{{X: (a.X + delta.X), Y: (a.Y + delta.Y)}, {X: (b.X - delta.X), Y: (b.Y - delta.Y)}}
+}
+
+func getAntinodeFrequencyLocations(a util.Coordinate, b util.Coordinate, bounds util.Coordinate) []util.Coordinate {
+	delta := getCoordinateDelta(a, b)
+	locations := []util.Coordinate{}
+	leftWise := util.Coordinate{X: 0, Y: 0}
+	multiplier := 0
+	for isWithinBounds(leftWise, bounds) {
+		leftWise = util.Coordinate{X: (a.X + (delta.X * multiplier)), Y: (a.Y + (delta.Y * multiplier))}
+		locations = append(locations, leftWise)
+		multiplier++
+	}
+	rightWise := util.Coordinate{X: 0, Y: 0}
+	multiplier = 0
+	for isWithinBounds(rightWise, bounds) {
+		rightWise = util.Coordinate{X: (b.X - (delta.X * multiplier)), Y: (b.Y - (delta.Y * multiplier))}
+		locations = append(locations, rightWise)
+		multiplier++
+	}
+	return locations
+}
+
+func parseAntennae(data string) (map[rune][]util.Coordinate, util.Coordinate) {
+	antennae := make(map[rune][]util.Coordinate)
+	lines := strings.Split(data, "\n")
+	mapBounds := util.Coordinate{Y: len(lines), X: len(lines[0])}
+	for y, line := range lines {
+		for x, char := range line {
+			if char == '.' { continue }
+			coords, ok := antennae[char]
+			position := util.Coordinate{X: x, Y: y}
+			if ok {
+				coords = append(coords, position)
+			} else {
+				coords = []util.Coordinate{position}
+			}
+			antennae[char] = coords
+		}
+	}
+	return antennae, mapBounds
 }
---
+++ 2024/days/day_08_test.go
@@ -0,0 +1,125 @@
+package days
+
+import (
+	"testing"
+
+	"github.com/Romanmc72/adventofcode/2024/util"
+)
+
+func TestPaseAndShowAntennae(t *testing.T) {
+	input := `............
+........0...
+.....0......
+.......0....
+....0.......
+......A.....
+............
+............
+........A...
+.........A..
+............
+............`
+	wantAntenae := map[rune][]util.Coordinate{
+		'0': {
+			{ X: 8, Y: 1},
+			{ X: 5, Y: 2},
+			{ X: 7, Y: 3},
+			{ X: 4, Y: 4},
+		},
+		'A': {
+			{X: 6, Y: 5},
+			{X: 8, Y: 8},
+			{X: 9, Y: 9},
+		},
+	}
+	wantBounds := util.Coordinate{X: 12, Y: 12}
+	gotAntennae, gotBounds := parseAntennae(input)
+	for a, wantedLocations := range wantAntenae {
+		gotLocations := gotAntennae[a]
+		for index, wantLocation := range wantedLocations {
+			gotLocation := gotLocations[index]
+			if wantLocation != gotLocation {
+				t.Errorf("Wanted=%s but got=%s location with wanted output=`%v` out of received output=`%v`", wantLocation, gotLocation, wantAntenae, gotAntennae)
+			}
+		}
+	}
+	if wantBounds != gotBounds {
+		t.Errorf("Wanted bounds=%s got bounds=%s", wantBounds, gotBounds)
+	}
+	wantShow := `#...........
+........0...
+.....0......
+.......0....
+....0.......
+......A.....
+............
+............
+........A...
+.........A..
+............
+...........#`
+	gotShow := showAntennae(gotAntennae, map[util.Coordinate]bool{{X: 0, Y:0}: true, {X: gotBounds.X - 1, Y: gotBounds.Y - 1}: true}, gotBounds)
+	if wantShow != gotShow {
+		t.Errorf("not showing antennae correctly wanted=\n%s\nbut got=\n%s\n", wantShow, gotShow)
+	}
+}
+
+func TestGetAntinodeLocations(t *testing.T) {
+	type testCase struct {
+		a util.Coordinate
+		b util.Coordinate
+		want []util.Coordinate
+		desc string
+	}
+	testCases := []testCase{
+		{
+			a: util.Coordinate{X: 0, Y: 0},
+			b: util.Coordinate{X: 1, Y: 1},
+			want: []util.Coordinate{{X: -1, Y: -1}, {X: 2, Y: 2}},
+			desc: "Straight line diagonally",
+		},
+		{
+			a: util.Coordinate{X: 2, Y: 5},
+			b: util.Coordinate{X: 4, Y: 6},
+			want: []util.Coordinate{{X: 0, Y: 4}, {X: 6, Y: 7}},
+			desc: "Over two, down 1",
+		},
+		{
+			a: util.Coordinate{X: 4, Y: 2},
+			b: util.Coordinate{X: 4, Y: 8},
+			want: []util.Coordinate{{X: 4, Y: -4}, {X: 4, Y: 14}},
+			desc: "Horizontal",
+		},
+		{
+			a: util.Coordinate{X: 7, Y: 20},
+			b: util.Coordinate{X: 7, Y: 4},
+			want: []util.Coordinate{{X: 7, Y: 36}, {X: 7, Y: -12}},
+			desc: "Vertical",
+		},
+	}
+	for _, tc := range testCases {
+		got := getAntinodeLocations(tc.a, tc.b)
+		if len(got) != 2 {
+			t.Errorf("That is a few too many points")
+		}
+		for index, wantedAntinode := range tc.want {
+			gotAntinode := got[index]
+			if wantedAntinode != gotAntinode {
+				t.Errorf("%s\nWanted=%s but got=%s for getAntinodeLocations(%s, %s)", tc.desc, wantedAntinode, gotAntinode, tc.a, tc.b)
+			}
+		}
+	}
+}
+
+
+func TestIsWithinBounds(t *testing.T) {
+	bottomRightBounds := util.Coordinate{X: 10, Y: 10}
+	inBounds := util.Coordinate{X: 5, Y: 5}
+	outOfBounds := util.Coordinate{X: 11, Y: 7}
+	if !isWithinBounds(inBounds, bottomRightBounds) {
+		t.Errorf("Wanted %s to be within bounds of %s but was NOT!", inBounds, bottomRightBounds)
+	}
+	if isWithinBounds(outOfBounds, bottomRightBounds) {
+		t.Errorf("Wanted %s to NOT be within bounds of %s but WAS!", outOfBounds, bottomRightBounds)
+	}
+}
