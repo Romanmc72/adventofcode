c1330180ff7acd834d82b961c23ba13bcec5d060
Roman Czerwinski <romanczerwinski@r0m4n.com>
2025-12-11 19:59:50 -0600

    2025: WIP, day 5 pt 1 done, working on pt 2

--- 2025/Sources/aoc/days/04.swift
+++ 2025/Sources/aoc/days/04.swift
@@ -1,9 +1,77 @@
+struct Coordinate {
+    let x: Int
+    let y: Int
+}
+
 struct Day04: DayChallenge {
+    func makeGrid(input: String) -> [[Int]] {
+        var grid: [[Int]] = []
+        for line in input.split(separator: "\n") {
+            var row: [Int] = []
+            for c in line {
+                row.append(c == "@" ? 1 : 0)
+            }
+            grid.append(row)
+        }
+        return grid
+    }
+
+    func identifyRemovals(grid: [[Int]]) -> [Coordinate] {
+        let accessibleIfLessThan = 4
+        var accessibleRolls: [Coordinate] = []
+        for (rowIndex, row) in grid.enumerated() {
+            for (colIndex, roll) in row.enumerated() {
+                if roll == 0 {
+                    continue
+                }
+
+                let leftBound = colIndex == 0
+                let rightBound = colIndex == row.count - 1
+                let upBound = rowIndex == 0
+                let downBound = rowIndex == grid.count - 1
+
+                let surroundings: [Int] = [
+                    leftBound || downBound ? 0 : grid[rowIndex + 1][colIndex - 1],
+                    leftBound ? 0 : grid[rowIndex][colIndex - 1],
+                    leftBound || upBound ? 0 : grid[rowIndex - 1][colIndex - 1],
+                    upBound ? 0 : grid[rowIndex - 1][colIndex],
+                    rightBound || upBound ? 0 : grid[rowIndex - 1][colIndex + 1],
+                    rightBound ? 0 : grid[rowIndex][colIndex + 1],
+                    rightBound || downBound ? 0 : grid[rowIndex + 1][colIndex + 1],
+                    downBound ? 0 : grid[rowIndex + 1][colIndex],
+                ]
+                let surroundingRollCount = surroundings.reduce(0, +)
+                if surroundingRollCount < accessibleIfLessThan {
+                    accessibleRolls.append(Coordinate(x: colIndex, y: rowIndex))
+                }
+            }
+        }
+        return accessibleRolls
+    }
+
+    func removeRolls(grid: [[Int]], coords: [Coordinate]) -> [[Int]] {
+        var copiedGrid = grid
+        for coord in coords {
+            copiedGrid[coord.y][coord.x] = 0
+        }
+        return copiedGrid
+    }
+
     func part1(input: String) -> Any {
-        return "Not implemented yet" 
+        let grid = makeGrid(input: input)
+        let removableRolls = identifyRemovals(grid: grid)
+        return removableRolls.count
     }
     
     func part2(input: String) -> Any {
-        return "Not implemented yet"
+        var totalRemoved = 0
+        var grid = makeGrid(input: input)
+        var removableRolls = identifyRemovals(grid: grid)
+        while removableRolls.count > 0 {
+            totalRemoved += removableRolls.count
+            grid = removeRolls(grid: grid, coords: removableRolls)
+            removableRolls = identifyRemovals(grid: grid)
+        }
+        return totalRemoved
     }
 }
--- 2025/Sources/aoc/days/05.swift
+++ 2025/Sources/aoc/days/05.swift
@@ -1,9 +1,86 @@
+struct Range {
+    var min: Int
+    var max: Int
+}
+
 struct Day05: DayChallenge {
+    func getRanges(input: [String.SubSequence]) -> [Range] {
+        var ranges: [Range] = []
+        for line in input {
+            let splitLine = line.split(separator: "-")
+            guard let min = Int(splitLine[0]) else {
+                print("[ERROR] Issue getting number from line! \(line)")
+                return []
+            }
+            guard let max = Int(splitLine[1]) else {
+                print("[ERROR] Issue getting number from line! \(line)")
+                return []
+            }
+            ranges.append(Range(min: min, max: max))
+        }
+        return ranges.sorted(by: {(r1: Range, r2: Range) -> Bool in return r1.min < r2.min })
+    }
+
+    func getIngredientIds(input: [String.SubSequence]) -> [Int] {
+        var ingredientIds: [Int] = []
+        for line in input {
+            guard let n = Int(line) else {
+                print("[ERROR] Error parsing ingredient id \(line)")
+                return []
+            }
+            ingredientIds.append(n)
+        }
+        return ingredientIds.sorted()
+    }
+
+    func consolidateRanges(ranges: [Range]) -> [Range] {
+        var consolidatedRanges: [Range] = []
+        var rangeIndex = 0
+        while rangeIndex < ranges.count {
+            var nextRange = rangeIndex + 1
+            let range = ranges[rangeIndex]
+            var nextRange = ranges[]
+        }
+        return consolidatedRanges
+    }
+
     func part1(input: String) -> Any {
-        return "Not implemented yet" 
+        let sections = input.split(separator: "\n\n")
+        let ranges = getRanges(input: String(sections[0]).split(separator: "\n"))
+        let ingredientIds = getIngredientIds(input: String(sections[1]).split(separator: "\n"))
+        var spoils = 0
+        ingredientLoop: for ingredientId in ingredientIds {
+            for range in ranges {
+                if ingredientId > range.min && ingredientId < range.max {
+                    spoils += 1
+                    continue ingredientLoop
+                }
+            }
+        }
+        return spoils
     }
     
     func part2(input: String) -> Any {
-        return "Not implemented yet"
+        let sections = input.split(separator: "\n\n")
+        let ranges = getRanges(input: String(sections[0]).split(separator: "\n"))
+        let consolidatedRanges = consolidateRanges(ranges: ranges)
+        var totalFreshRanges = 0
+        for range in consolidatedRanges {
+            totalFreshRanges += range.max - range.min
+        }
+        return totalFreshRanges
     }
 }
+
+// cases to consider for consolidating the array
+// [    ]
+// [    ]
+
+// [    ]
+//   [    ]
+
+// [    ]
+//   []
+
+// [    ]
+//        [   ]
